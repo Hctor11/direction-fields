d7cac056fb659c8e33f0ba394d2c74830882e5e1
hector <hectorrrivera1010@gmail.com>
2023-06-27 20:04:42 -0600

    add full vh and vw in the cartesian plane

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,9 +1,15 @@
 import { Mafs, Coordinates } from "mafs"
 import './Styles/Styles.css'
+ 
+const height = window.innerHeight;
+const width = window.innerWidth
 
 const Graph = () => {
   return (
-    <Mafs>
+    <Mafs 
+        height={height}
+        width={width}
+        zoom={{min: 0.1, max: 1}}>
         <Coordinates.Cartesian/>
     </Mafs>
   )
